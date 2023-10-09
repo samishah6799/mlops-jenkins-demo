f2efab9074fb9c40399d3c62618213e65367b512
sami ullah shah <samishah121442@gmail.com>
2023-10-09 12:24:28 +0500

    Second commit

--- test.py
+++ test.py
@@ -7,3 +7,6 @@ def test_bulb_isOff():
 def test_bulb_isOn():
     bulb.isOn()
     assert bulb.getStatus() == "Bulb is glowing"
+
+def test_getStatus():
+    assert bulb.getStatus() == "Bulb is glowing"
